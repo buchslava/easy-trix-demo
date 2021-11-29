d06f6074a2b0e7da30c480ed8ca7eca4cbdc7aec
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2021-11-29 17:27:11 +0200

    toolbar functions

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,22 @@
 import Trix from 'trix';
 import { TagTide } from 'tag-tide';
 
+Trix.config.blockAttributes.default.tagName = 'p';
+Trix.config.textAttributes.foo = {
+  style: { color: 'green', backgroundColor: 'yellow' },
+  parser: function (element: any) {
+    return element.style.color === 'green' && element.style.backgroundColor === 'yellow';
+  },
+  inheritable: true,
+};
+Trix.config.textAttributes.red = {
+  style: { color: 'red' },
+  parser: function (element: any) {
+    return element.style.color === 'red';
+  },
+  inheritable: true,
+};
+
 function debounce(func: Function, timeout = 300) {
   let timer: any;
   return (...args: any) => {
@@ -13,39 +29,59 @@ function debounce(func: Function, timeout = 300) {
   };
 }
 
-function calc() {
-  const calcEl = document.getElementById('calc-container');
-  if (calcEl) {
-    calcEl.innerHTML = `Words: ${editor.value.split(/\s+/).length-1}`;
-  }
-}
-
-const debouncedCalc = debounce(calc, 400);
-
-let editor: any;
-
 document.addEventListener('trix-initialize', function (event) {
-  Trix.config.blockAttributes.default.tagName = 'p';
+  const trix: any = document.getElementById('trix-editor');
 
-  editor = document.querySelector('trix-editor');
+  const debouncedCalc = debounce(() => {
+    const calcEl = document.getElementById('calc-container');
+    if (calcEl) {
+      calcEl.innerHTML = `Words: ${trix.value.split(/\s/).length - 1}`;
+    }
+  }, 400);
 
-  editor.addEventListener('trix-paste', (e: any) => {
+  // toolbar events
+  const dataFormats = document.querySelectorAll('[data-format]');
+
+  function formatText(format: any) {
+    const { editor } = trix;
+    const { currentAttributes } = editor.composition;
+
+    if (currentAttributes[format]) {
+      editor.deactivateAttribute(format);
+    } else {
+      editor.activateAttribute(format);
+    }
+  }
+
+  function handleMouseDown(e: any) {
+    e.preventDefault();
+    //@ts-ignore
+    formatText(this.getAttribute('data-format'));
+  }
+
+  for (let i = dataFormats.length; i--; ) {
+    dataFormats[i].addEventListener('mousedown', handleMouseDown);
+  }
+  // /toolbar events
+  debouncedCalc();
+
+  trix.editor.insertHTML(`<p>Hello world</p>`);
+  trix.addEventListener('trix-paste', (e: any) => {
     const prettified = new TagTide(e.paste.html)
       .startAfter('id', /^docs-internal-guid-.+/)
       .textTable()
-      .flatten(['a', 'img', 'li'])
+      .flatten(['p', 'a', 'img', 'li', 'h1', 'h2', 'h3', 'h4', 'h5'])
       .removeAttributes({ a: ['href', '_target'], img: ['src', 'alt'] })
       .rootParagraphs()
-      .result(['meta']);
+      .result(['meta', 'img']);
 
-    // console.log(e.paste.html);
-    // console.log(prettified);
+    console.log(e.paste.html);
+    console.log(prettified);
 
-    editor.editor.undo();
-    editor.editor.insertHTML(prettified);
+    trix.editor.undo();
+    trix.editor.insertHTML(prettified);
   });
-
-  editor.addEventListener('trix-change', (e: any) => {
+  trix.addEventListener('trix-change', (e: any) => {
     debouncedCalc();
   });
 });
