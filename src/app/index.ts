ff4b5faf92072018d11fac6bf30d2feb62f9a00d
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2021-11-29 15:14:47 +0200

    it works

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import Trix from 'trix';
-import { TagTide } from "tag-tide";
+import { TagTide } from 'tag-tide';
 
 function debounce(func: Function, timeout = 300) {
   let timer: any;
@@ -14,7 +14,10 @@ function debounce(func: Function, timeout = 300) {
 }
 
 function calc() {
-  console.log(editor.value);
+  const calcEl = document.getElementById('calc-container');
+  if (calcEl) {
+    calcEl.innerHTML = `Words: ${editor.value.split(/\s+/).length}`;
+  }
 }
 
 const debouncedCalc = debounce(calc, 400);
@@ -28,12 +31,15 @@ document.addEventListener('trix-initialize', function (event) {
 
   editor.addEventListener('trix-paste', (e: any) => {
     const prettified = new TagTide(e.paste.html)
-      .startAfter("id", /^docs-internal-guid-.+/)
+      .startAfter('id', /^docs-internal-guid-.+/)
       .textTable()
-      .flatten(["a", "img", "li"])
-      .removeAttributes({ a: ["href", "_target"], img: ["src", "alt"] })
+      .flatten(['a', 'img', 'li'])
+      .removeAttributes({ a: ['href', '_target'], img: ['src', 'alt'] })
       .rootParagraphs()
-      .result();
+      .result(['meta']);
+
+    // console.log(e.paste.html);
+    // console.log(prettified);
 
     editor.editor.undo();
     editor.editor.insertHTML(prettified);
